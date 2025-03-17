cd94236c9df6d5897f843b558bf23aea864818be
Greg Ells <gregells@gmail.com>
2025-03-17 16:00:44 -0400

    Add users-api utility and code signUp function

---
+++ src/utilities/users-api.js
@@ -0,0 +1,18 @@
+// This is the base path of the Express route:
+const BASE_URL = '/api/users'
+
+export async function signUp(userData) {
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData)
+  });
+  
+  // Check if request was successful (status code in the 200s):
+  if (res.ok) {
+    // res.json() will resolve to the JWT
+    return res.json();
+  } else {
+    throw new Error('Invalid Sign Up');
+  }
+}
