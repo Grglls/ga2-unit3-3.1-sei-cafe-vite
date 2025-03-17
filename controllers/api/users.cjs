4dc2b9a769c6c8966c42a6cf691ca3956ea87fb2
Greg Ells <gregells@gmail.com>
2025-03-17 18:22:37 -0400

    Adjust crud-helper for commonjs file extensions

--- controllers/api/users.cjs
+++ controllers/api/users.cjs
@@ -1,12 +1,13 @@
+const User = require('../../models/user');
+
 module.exports = {
   create,
 };
 
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 }
--- crud-helper.cjs
+++ crud-helper.cjs
@@ -1,9 +1,9 @@
 // Connect to the database
 require('dotenv').config();
-require('./config/database');
+require('./config/database.cjs');
 
 // Require the Mongoose models
-// const User = require('./models/user');
+const User = require('./models/user.cjs');
 // const Item = require('./models/item');
 // const Category = require('./models/category');
 // const Order = require('./models/order');
