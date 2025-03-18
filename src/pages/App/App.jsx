6282427d218503992bf20eca345e2a0b2ef4e725
Greg Ells <gregells@gmail.com>
2025-03-18 19:45:21 -0400

    Use localStorage token to set initial value of user state

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
 import AuthPage from '../AuthPage/AuthPage';
@@ -7,7 +8,7 @@ import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
