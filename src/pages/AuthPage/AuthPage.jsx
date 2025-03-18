8d3d4b7491745fd642623fd6fa901ca360bd1931
Greg Ells <gregells@gmail.com>
2025-03-18 15:00:39 -0400

    Add LoginForm and pass setUser down, add button to toggle between LoginForm & SignUpForm

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,21 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import { useState } from "react";
 
-export default function AuthPage () {
+export default function AuthPage ({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
     <main>
       <h1>AuthPage</h1>
-      <SignUpForm />
+      <button onClick={() => setShowLogin(!showLogin)} >
+        { showLogin ? 'Sign Up' : 'Log In' }
+      </button>
+      { showLogin ? 
+          <LoginForm setUser={setUser} />
+          :
+          <SignUpForm />
+      }
     </main>
   );
 }
