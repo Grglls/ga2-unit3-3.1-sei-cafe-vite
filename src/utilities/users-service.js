47db7765e1d1445ba6ad58f4d8485a862eb26c85
Greg Ells <gregells@gmail.com>
2025-03-18 20:27:01 -0400

    Update 'login' service function to save token to localStorage and return the user data from the token

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -14,7 +14,8 @@ export async function signUp(userData) {
 export async function login(credentials) {
   // Delegate the AJAX request to the users-api.js module.
   const token = await usersAPI.login(credentials);
-  return token;
+  localStorage.setItem('token', token);
+  return getUser();
 }
 
 export function logOut() {
