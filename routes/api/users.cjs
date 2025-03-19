1161d8e30f5f6af76b2266d5663b20d167a10266
Greg Ells <gregells@gmail.com>
2025-03-19 15:49:50 -0400

    Protect check-token route with ensureLoggedIn middleware

--- routes/api/users.cjs
+++ routes/api/users.cjs
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users.cjs');
+const ensureLoggedIn = require('../../config/ensureLoggedIn.cjs');
 
 // All routes start with /api/users
 
 // GET /api/users/check-token
-router.get('/check-token', usersCtrl.checkToken);
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 // POST /api/users
 router.post('/', usersCtrl.create);
