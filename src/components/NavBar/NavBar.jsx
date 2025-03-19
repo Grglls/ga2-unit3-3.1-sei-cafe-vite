6019eb2216ad76577919b7159dedecbf078cc316
Greg Ells <gregells@gmail.com>
2025-03-18 20:12:18 -0400

    Add link to logout and code handler function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom"
+import * as usersService from "../../utilities/users-service"
 
-export default function NavBar({ user }) {
+export default function NavBar({ user, setUser }) {
+  function handleLogOut() {
+    // Delegate to the users-service:
+    usersService.logOut();
+    setUser(null);
+  } 
   return (
     <nav>
       <Link to="/orders">Order History</Link>
@@ -8,6 +14,9 @@ export default function NavBar({ user }) {
       <Link to="/orders/new">New Order</Link>
       &nbsp; | &nbsp;
       <span>Welcome, {user.name}</span>
+      &nbsp; | &nbsp;
+      {/* 'Log Out' could be a Link or a button depending on desired look. */}
+      <Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   )
 }
