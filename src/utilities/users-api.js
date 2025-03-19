450caf5e17b537e135e4154a1ae4a99a99162366
Greg Ells <gregells@gmail.com>
2025-03-19 15:18:32 -0400

    Add sendRequest module and refactor users-api functions to make could more DRY

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,33 +1,12 @@
+import sendRequest from "./send-request";
 // This is the base path of the Express route:
-const BASE_URL = '/api/users'
+const BASE_URL = '/api/users';
 
 export async function signUp(userData) {
-  const res = await fetch(BASE_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(userData)
-  });
-  
-  // Check if request was successful (status code in the 200s):
-  if (res.ok) {
-    // res.json() will resolve to the JWT
-    return res.json();
-  } else {
-    throw new Error('Invalid Sign Up');
-  }
+  return sendRequest(BASE_URL, 'POST', userData);
 }
 
 export async function login(credentials) {
-  const res = await fetch(`${BASE_URL}/login`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials)
-  });
-
-  // Check if request was successful (status code in the 200s):
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Invalid Log In');
-  }
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+}
 }
