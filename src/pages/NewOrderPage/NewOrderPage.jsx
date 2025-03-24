f6be6c766fcd6e8054858ad00af2019f1bc65433
Greg Ells <gregells@gmail.com>
2025-03-24 15:31:36 -0400

    Add state variable for menuItems, retrieve data from server on first render

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,18 @@
-export default function NewOrderPage () {
+import { useState, useEffect } from "react";
+import * as itemsAPI from "../../utilities/items-api";
+
+export default function NewOrderPage() {
+  const [ menuItems, setMenuItems ] = useState([]);
+
+  // Empty dependency array causes the effect to run only after the first render:
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);
+
   return (
     <h1>NewOrderPage</h1>
   );
