7f82beccab326674ad1c651ad782dcec1a978cda
Greg Ells <gregells@gmail.com>
2025-03-28 13:49:17 -0400

    Update OrderHistoryPage to pull orders from database and render OrderList component

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,42 @@
-export default function OrderHistoryPage() {
+import { useState, useEffect } from 'react';
+import * as ordersAPI from '../../utilities/orders-api';
+import './OrderHistoryPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import OrderList from '../../components/OrderList/OrderList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function OrderHistoryPage({ user, setUser }) {
+  const [orders, setOrders] = useState([]);
+  const [activeOrder, setActiveOrder] = useState(null);
+
+  // The empty dependency array causes the effect
+  // to run ONLY after the FIRST render
+  useEffect(function() {
+    async function getOrders() {
+      const orders = await ordersAPI.getAll();
+      setOrders(orders);
+      setActiveOrder(orders[0]);
+    }
+    getOrders();
+  }, []);
+
   return (
-    <>
-      <h1>OrderHistoryPage</h1>
-    </>
+    <main className="OrderHistoryPage">
+      <aside>
+        <Logo />
+        <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <OrderList
+        orders={orders}
+        activeOrder={activeOrder}
+        setActiveOrder={setActiveOrder}
+      />
+      <OrderDetail
+        order={activeOrder}
+      />
+    </main>
   );
 }
